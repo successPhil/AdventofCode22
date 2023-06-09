e2d64eb30e04f8482c37da35081641412954aa1c
successPhil <129565748+successPhil@users.noreply.github.com>
2023-06-09 18:11:56 -0400

    advent day06 part2

---
+++ day06-2py/main.py
@@ -0,0 +1,21 @@
+def find_marker(filename):
+  """This function reads a file by line and slices each line
+  to get 14 characters, then compares them to a set of the same characters
+  to see if they are distinct
+  Return: The amount of characters processed until 4 distinct chars are seen"""
+  chars_processed = 0
+  with open(filename, 'r') as datastream:
+    for line in datastream:
+      line = line.strip()
+      for i in range(len(line)-14):
+        marker = line[i:i+14]
+        if len(marker) == len(set(marker)):
+          chars_processed += i + 14
+          return chars_processed  
+        # elif i == len(line)-5:
+        #   chars_processed += len(line)
+        #Only needed if data is on multiple lines
+  
+
+
+print(find_marker('datastream.txt'))
